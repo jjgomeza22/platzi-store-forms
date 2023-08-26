5b82101f869e7807e88351a24984e889174d820f
Juan José Gómez A <jjgomeza22@gmail.com>
2023-08-26 14:33:49 -0500

    Add component basic-form and use formControl to use reactive forms

---
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-basic-form',
+  templateUrl: './basic-form.component.html',
+  styleUrls: ['./basic-form.component.scss']
+})
+export class BasicFormComponent implements OnInit {
+
+  nameField = new FormControl(' soy un control');
+
+  ngOnInit(): void {
+    this.nameField.valueChanges
+    .subscribe(value => {
+      console.log(value);
+    });
+  }
+
+  getNameFliedValue() {
+    console.log(this.nameField.value);
+  }
+
+}
