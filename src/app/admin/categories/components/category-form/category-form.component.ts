866e267aed248b5e08272115249a16e46e42adc0
Juan José Gómez A <jjgomeza22@gmail.com>
2023-08-27 10:26:42 -0500

    Category service and validations in the form

--- src/app/admin/categories/components/category-form/category-form.component.ts
+++ src/app/admin/categories/components/category-form/category-form.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { CategoriesService } from '../../../../core/services/categories.service';
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.scss']
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent {
 
-  constructor() { }
+  public form: FormGroup;
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private categoriesService: CategoriesService,
+    private router: Router
+  ) {
+    this.buildForm();
+   }
+
+  private buildForm(): void {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      image: ['', Validators.required]
+    });
   }
 
+  public save(): void {
+    if (this.form.valid) {
+      this.createCategory();
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  private createCategory() {
+    const data = this.form.value;
+    this.categoriesService.createCategory(data)
+    .subscribe(() => {
+      this.router.navigate(['./admin/categories']);
+    });
+  }
+
+  get name() {
+    return this.form.get('name');
+  }
+
+  get image() {
+    return this.form.get('image');
+  }
 }
---
+++ src/app/core/services/categories.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Category } from '../models/category.model';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  public getAllCategories() {
+    return this.http.get<Category[]>(`${environment.url_api}/categories/`)
+  }
+
+  public createCategory(data: Partial<Category>) {
+    return this.http.post<Category[]>(`${environment.url_api}/categories/`, data);
+  }
+
+  public updateCategory(id: string, data: Partial<Category>) {
+    return this.http.put<Category[]>(`${environment.url_api}/categories/${id}`, data);
+  }
+}
